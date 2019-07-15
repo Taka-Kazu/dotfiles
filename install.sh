83c5dfa52bcb067615f86bfc8c1169f2ccc1024c
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-07-15 15:16:58 +0900

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim --depth=1
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
+vim_version=`vim --version | grep -o '[0-9]*'`
+vim_version=${vim_version:0:1}
+
+if [ $((${vim_version})) -lt 8 ]; then
+    echo "installed vim version < 8"
+    cd ~/.vim/dein/repos/github.com/Shougo/dein.vim
+    git checkout 1.5
+    cd $SCRIPT_DIR
+else
+    echo "installed vim version >= 8"
+fi
 
 mkdir ~/.vim/undo
+
+echo "source $SCRIPT_DIR/vimrc" >> ~/.vimrc
 
 echo "open vim and run ':call dein#install()'"
